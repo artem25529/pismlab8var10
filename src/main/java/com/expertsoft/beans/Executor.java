180c645434b9b8385b02392fee53972f39796461
Artem <martinovich.artem02@mail.ru>
2022-04-27 23:09:30 +0300

    Initial commit

---
+++ src/main/java/com/expertsoft/beans/Executor.java
@@ -0,0 +1,29 @@
+package com.expertsoft.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class Executor {
+    private String target;
+
+    public Executor() {
+
+    }
+
+    public String execute() {
+        target = new StringBuilder(target)
+                .reverse()
+                .toString();
+        return "result";
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+}
